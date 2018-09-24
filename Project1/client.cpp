05c60c7ed49269aeb4e52ed9d8b463dba004014c
lawtonan <lawtonan@mail.gvsu.edu>
2018-09-24 11:14:36 -0400

    Removed Obselete code and directories

--- Project1/client.cpp
+++ Project1/client.cpp
@@ -15,19 +15,19 @@ int main(int argc, char** argv) {
         std::cout << "There was a problem creating the socket\n";
         return 1;
     }
-    
+
     char ipAddress[5000];
     int port;
     std::cout << "Enter an IP address: ";
     std::cin >> ipAddress;
     std::cout << "Enter a Port number: ";
     std::cin >> port;
-    
+
     struct sockaddr_in serveraddr;
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(port);
     serveraddr.sin_addr.s_addr= inet_addr(ipAddress);
-    
+
     char line[5000];
     char fout[5000];
     //std::string input;
@@ -37,17 +37,17 @@ int main(int argc, char** argv) {
     std::cout << "Enter a file to write to: ";
     std::cin.getline(fout,5000);
 
-    
+
     std::FILE* myfile = std::fopen(fout,"w");
     if(myfile == NULL){
         return 1;
     }
-    
+
     char line2[1024];
     int len = sizeof(serveraddr);
     sendto(sockfd,line,strlen(line)+1,0,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
     // recvfrom(sockfd, line2, 5000, 0, (struct sockaddr*)&serveraddr,(socklen_t*)&len);
-    
+
     int packets = 1;
     int recieve = 1024;
     char pcount[100];
@@ -55,7 +55,7 @@ int main(int argc, char** argv) {
     std::size_t fw;
 
     while(1){
-        
+
         recieve = recvfrom(sockfd,line2,1024,0,(struct sockaddr*)&serveraddr,(socklen_t*)&len);
         if (strcmp(line2,"EOF") == 0) {
             std::cout << "End of file reached.\n";
@@ -67,24 +67,24 @@ int main(int argc, char** argv) {
         }
         if(recieve != -1){
             //	pos = strlen(line2) - 1;
-            if(recieve != 1024){
-                line2[recieve] = EOF;
-            }
+            // if(recieve != 1024){
+            //     line2[recieve] = EOF;
+            // }
             fw = fwrite(line2,sizeof(char),recieve,myfile);
-        }			
-        
+        }
+
         std::cout << "Packets recieved: " << packets << "\n";
         packets++;
         // want to send the number of the packet recieved back to the server
         //		pcount = string(itoa(packets));
         // send a message to server saying the the packet was recieved
-        sendto(sockfd,pcount,strlen(pcount)+1,0,(struct sockaddr*)&serveraddr,sizeof(serveraddr));	
-        
+        sendto(sockfd,pcount,strlen(pcount)+1,0,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+
     }
-    
+
     fclose(myfile);
-    
+
     close(sockfd);
-    
+
     return 0;
 }
